8425caea8266a7626ac0e3f46909f68a5370ce3b
Kofo Okesola <okesolakofo@gmail.com>
2021-04-21 16:45:57 +0100

    graceful shutdown

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"os/signal"
 
 	"github.com/kofoworola/tunnelify/config"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -12,22 +14,38 @@ var (
 	app        = kingpin.New("proxify", "A lightweight and easily deployable proxy server written in go")
 	start      = app.Command("start", "start the proxify server.")
 	configFile = start.Arg("config", "config file to start proxify server with.").String()
+
+	verify = app.Command("verify", "validate the configuration file")
 )
 
 func main() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-ch
+		cancel()
+	}()
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
 	case start.FullCommand():
-	}
-	config, err := config.LoadConfig(*configFile)
-	if err != nil {
-		panic(err)
-	}
-	proxy, err := NewServer(config)
-	if err != nil {
-		panic(err)
-	}
+		config, err := config.LoadConfig(*configFile)
+		if err != nil {
+			panic(err)
+		}
+		server, err := NewServer(config)
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			fmt.Println("server starting...")
+			if err := server.Start(); err != nil {
+				panic(err)
+			}
+		}()
 
-	if err := proxy.Start(context.Background()); err != nil {
-		panic(err)
+		<-ctx.Done()
+		fmt.Println("shutting down server...")
+		server.Shutdown()
 	}
 }
--- tunnelify.go
+++ tunnelify.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
 	"net"
 	"strings"
@@ -16,10 +15,9 @@ import (
 // TODO proxy authentication
 
 type Server struct {
-	config       *config.Config
-	listener     net.Listener
-	logger       *logging.Logger
-	cleanupFuncs []func() error
+	config   *config.Config
+	listener net.Listener
+	logger   *logging.Logger
 }
 
 func NewServer(cfg *config.Config) (*Server, error) {
@@ -34,55 +32,49 @@ func NewServer(cfg *config.Config) (*Server, error) {
 	}
 
 	return &Server{
-		listener:     listener,
-		config:       cfg,
-		logger:       logger,
-		cleanupFuncs: []func() error{listener.Close},
+		listener: listener,
+		config:   cfg,
+		logger:   logger,
 	}, nil
 }
 
-func (p *Server) Start(ctx context.Context) error {
-	// listen to new connections
-listenLoop:
-	for {
-		select {
-		case <-ctx.Done():
-			break listenLoop
-		default:
-			c, err := p.listener.Accept()
-			var h handler.ConnectionHandler
-			if err != nil {
-				p.logger.LogError("error accepting a new connection", err)
-				break
-			}
-			// read first line of the connection and use an appropriate handler
-			r := bufio.NewReader(c)
-			reqLine, err := r.ReadBytes('\n')
-			if err != nil {
-				p.logger.LogError("error reading request line from connection", err)
-				continue
-			}
+func (s *Server) Shutdown() {
+	s.listener.Close()
 
-			// check the reqline for the handler to use
-			reqDetails := strings.Split(string(reqLine), " ")
-			if len(reqDetails) != 3 {
-				p.logger.LogError("invalid request start line", nil)
-				continue
-			}
-
-			logger := p.logger.With("action", reqDetails[0])
-			cr := NewConnectionReader(reqLine, r, c)
-			if reqDetails[0] == "CONNECT" {
-				h = handler.NewTunnelHandler(p.config, cr, reqDetails[1], strings.TrimSpace(reqDetails[2]), c.Close)
-			} else if reqDetails[0] != "CONNECT" && !strings.HasPrefix("/", reqDetails[1]) {
-				h = handler.NewProxyHandler(cr, c.RemoteAddr().String(), p.config, c.Close)
-			}
-			go h.Handle(logger)
-		}
-	}
-	return nil
 }
 
-func (p *Server) addToCleanups(cleanupFunc func() error) {
-	p.cleanupFuncs = append(p.cleanupFuncs, cleanupFunc)
+func (s *Server) Start() error {
+	// listen to new connections
+	for {
+		c, err := s.listener.Accept()
+		var h handler.ConnectionHandler
+		if err != nil {
+			s.logger.LogError("error accepting a new connection", err)
+			break
+		}
+		// read first line of the connection and use an appropriate handler
+		r := bufio.NewReader(c)
+		reqLine, err := r.ReadBytes('\n')
+		if err != nil {
+			s.logger.LogError("error reading request line from connection", err)
+			continue
+		}
+
+		// check the reqline for the handler to use
+		reqDetails := strings.Split(string(reqLine), " ")
+		if len(reqDetails) != 3 {
+			s.logger.LogError("invalid request start line", nil)
+			continue
+		}
+
+		logger := s.logger.With("action", reqDetails[0])
+		cr := NewConnectionReader(reqLine, r, c)
+		if reqDetails[0] == "CONNECT" {
+			h = handler.NewTunnelHandler(s.config, cr, reqDetails[1], strings.TrimSpace(reqDetails[2]), c.Close)
+		} else if reqDetails[0] != "CONNECT" && !strings.HasPrefix("/", reqDetails[1]) {
+			h = handler.NewProxyHandler(cr, c.RemoteAddr().String(), s.config, c.Close)
+		}
+		go h.Handle(logger)
+	}
+	return nil
 }
